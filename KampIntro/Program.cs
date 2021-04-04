655d72bf95956eabd4187a9e8e9c6194c05e8b79
MehmetKandamar <72360607+MehmetKandamar@users.noreply.github.com>
2021-04-04 23:58:33 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety = tip güvenliği
+            //Do not repeat yourself = Kendini tekrarlama
+            //Değer tutucu, alias
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
+
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
